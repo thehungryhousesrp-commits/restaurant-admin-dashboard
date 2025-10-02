ca8296e89ddbe39dc005f3d4ba5d69dff327668c
Thehungry house <thehungryhouse.srp@gmail.com>
2025-10-02 06:54:37 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    allowedDevOrigins: [
-        "https://6000-firebase-studio-1758348122060.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev",
-    ]
-  },
+  allowedDevOrigins: [
+      "https://6000-firebase-studio-1758348122060.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev",
+  ],
   images: {
     remotePatterns: [
       {
