09878afe9e6f7a5be95021924dfdb12f9436648a
Thehungry house <thehungryhouse.srp@gmail.com>
2025-10-02 20:46:13 +0000

    The error "Unexpected token Card. Expected jsx identifier" in BulkUpload

--- src/components/admin/BulkUploader.tsx
+++ src/components/admin/BulkUploader.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import React from "react";
 import { useState } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -356,5 +357,3 @@ Veg Fried Rice: (Single) 130 | (Full) 180
     </Card>
   );
 }
-
-    
