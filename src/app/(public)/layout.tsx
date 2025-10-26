9fcf032bc5c7f0439483d79795058c7e408fd1d0
Thehungry house <thehungryhouse.srp@gmail.com>
2025-10-26 09:20:36 +0000

    404 error koi v target path nahi khiul raha hai jaise eg /dashboard,/das

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,3 +1,4 @@
+
 // This is a special layout for public-facing pages like the invoice.
 // It deliberately does NOT include the main <Header> component
 // to prevent customers from navigating to other parts of the app.
