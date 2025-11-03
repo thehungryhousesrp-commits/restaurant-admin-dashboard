dcbee381d6e70097e91ac32d9cb499c4d1790dce
Thehungry house <thehungryhouse.srp@gmail.com>
2025-11-03 16:48:50 +0000

    jab hum export karte hai data to selected me k sarein seriaal wise sare

--- src/components/order/OrderSummary.tsx
+++ src/components/order/OrderSummary.tsx
@@ -10,6 +10,7 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 
 interface OrderSummaryProps {
+  orderItems: OrderItem[];
   onUpdateOrder: (updatedOrder: OrderItem[]) => void;
 }
 
